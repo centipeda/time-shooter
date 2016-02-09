73d451573ec769d9ea63b5e31875646f207007bf
j-crit <joshua.c.guam@gmail.com>
2016-02-09 20:15:06 +1000

    Whole-sale rewrite to use pygame.sprite.Sprite objects.

--- time-shooter.py
+++ time-shooter.py
@@ -22,24 +22,12 @@ def main():
 
     fpsClock = pygame.time.Clock()
 
-    # set up screenx
+    # set up screen
     SCREEN = pygame.display.set_mode((WINWIDTH,WINHEIGHT))
     pygame.display.set_caption('Time-Shooter')
     SCREEN.fill(BGCOLOR)
 
-    # starting position for ship
-    playerShip = Ship(((WINWIDTH / 2) - (SHIPH / 2)),
-                      WINHEIGHT - SHIPH)
-    playerShip.draw(SCREEN)
-
-    # Mob list, used for updating
-    allMobs = []
-    moved = []
-    allMobs.append(playerShip)
-    moved.append(playerShip)
-
     while True: # main event loop
-        to_update = []
         keystate = pygame.key.get_pressed()
 
         # Handling for events from the event queue
@@ -48,91 +36,8 @@ def main():
                (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 pygame.quit()
                 sys.exit()
-        
-        ## Sets game-wide time-slowing variable
-        if keystate[pygame.K_SPACE]:
-            timeSlow = True
-            for mob in allMobs:
-                mob.slow = timeSlow
-        else:
-            timeSlow = False
-            for mob in allMobs:
-                mob.slow = timeSlow
 
-        ## Ship controls ##
-
-        # Ship movement controls, keeps ship in bounds
-        if keystate[pygame.K_LEFT] and \
-        playerShip.in_bounds('left',WINAREA):
-            playerShip.set_velx(playerShip.defspeed * -1)
-        elif keystate[pygame.K_RIGHT] and \
-            playerShip.in_bounds('right',WINAREA):
-            playerShip.set_velx(playerShip.defspeed)
-        else:
-            playerShip.set_velx(0)
-
-        if keystate[pygame.K_UP] and \
-            playerShip.in_bounds('top',WINAREA):
-            playerShip.set_vely(playerShip.defspeed)
-        elif keystate[pygame.K_DOWN] and \
-            playerShip.in_bounds('bottom',WINAREA):
-            playerShip.set_vely(playerShip.defspeed * -1)
-        else:
-            playerShip.set_vely(0)
-
-        if keystate[pygame.K_LSHIFT] or keystate[pygame.K_RSHIFT]:
-            # Keeps bullet interval fixed
-            rightNow = pygame.time.get_ticks()
-            if rightNow - playerShip.timeNow > BULDELAY:
-                b = playerShip.fire_bullet(SCREEN)
-                # b.color = random_color()
-                allMobs.append(b)
-                playerShip.timeNow = rightNow
-        
-        moved.append(playerShip)
-        ## End ship controls ##
-
-        ## Enemy behavior ##
-        ## End enemy behavior ##
-    
-        ##Testing##
-        if keystate[pygame.K_s]:
-            t = Enemy(200,300)
-            t.draw(SCREEN)
-            t.behavior = "straightright"
-            allMobs.append(t)
-        ##Testing##
-
-        ## Update all positions, velocities
-
-        # If mob ofscreen, delete it
-        for mob in allMobs:
-            if not mob.in_area(WINAREA):
-                allMobs.remove(mob)
-                
-            # Moves Enemy instances according to ai_accel()
-            if isinstance(mob,Enemy):
-                mob.ai_accel()
-                b = [t.rect for t in allMobs if isinstance(t,Bullet)]
-                if mob.rect.collidelist(b) != -1:
-                    corpse = mob.rect
-                    allMobs.remove(mob)
-                    SCREEN.fill(BGCOLOR,rect=corpse)
-                b = []
-            # Erases mob, then redraws in new area
-            SCREEN.fill(BGCOLOR,rect=mob.rect)
-            to_update.append(mob.rect)
-            mob.move()
-            mob.draw(SCREEN)
-            to_update.append(mob.rect)
-
-        pygame.display.update(to_update)
-        to_update = []
-        
-        # Prints all of the current mobs on-screen to terminal.
-        # Careful, outputs a LOT.
-        print allMobs
-
+        pygame.display.update()
         fpsClock.tick(FPS)
 
 if __name__ == '__main__':
--- tsclass.py
+++ tsclass.py
@@ -5,136 +5,20 @@ import pygame
 
 class Mob():
     """Base class for all mobile objects in time-shooter."""
-    def __init__(self,posx,posy):
-        self.posx = posx
-        self.posy = posy
-        self.height = None
-        self.width = None
-        self.velx = 0
-        self.vely = 0
-        self.defspeed = 0
-        self.rect = None
-        self.color = None
-        self.slow = False
-        self.slowfactor = SLOWFACTOR
-    
-    def draw(self,surf):
-        """Sets the rect attribute to the current position of the Mob."""
-        self.rect = pygame.Rect(self.posx,self.posy,
-                                self.width,self.height)
-        pygame.draw.rect(surf,self.color,self.rect)
-
-    def move(self):
-        """Adjusts posx and posy based on velx and vely.
-        Affected by slow attribute."""
-        if self.slow:
-            self.posx += self.velx / self.slowfactor
-            self.posy += ((-1 * self.vely) / self.slowfactor)
-        else:
-            self.posx += self.velx
-            self.posy += (-1 * self.vely)
-    
-    def in_bounds(self,side,bounds):
-        """Checks whether Mob is within a certain boundary."""
-        if side == 'left':
-            if self.rect.left > bounds.left:
-                return True
-        elif side == 'right':
-            if self.rect.right < bounds.right:
-                return True
-        elif side == 'top':
-            if self.rect.top > bounds.top:
-                return True
-        elif side == 'bottom':
-            if self.rect.bottom < bounds.bottom:
-                return True
-        else:
-            return False
-
-    def in_area(self,rect):
-        return self.rect.colliderect(rect)
-
-    def set_velx(self,velocity):
-        self.velx = velocity
-
-    def set_vely(self,velocity):
-        self.vely = velocity
+    def __init__(self):
+        pass
 
 class Ship(Mob):
     """Class for the player ship."""
-    def __init__(self,posx,posy):
-        Mob.__init__(self,posx,posy)
-        self.height = SHIPH
-        self.width = SHIPW
-        self.color = WHITE
-        self.defspeed = 6
-        self.slowfactor = 1
-        self.timeNow = pygame.time.get_ticks()
+    def __init__(self):
+        pass
 
-    def fire_bullet(self,surf):
-        """Spawns a Bullet object in front of the Ship."""
-        bullet = Bullet(self.rect.center[0],self.rect.top + 10)
-        bullet.draw(surf)
-        bullet.set_vely(bullet.defspeed)
-        return bullet
-    
 class Enemy(Mob):
     """Base class for all enemies."""
-    def __init__(self,posx,posy):
-        Mob.__init__(self,posx,posy)
-        self.width = 30
-        self.height = 30
-        self.color = GREEN
-        self.behavior = None
-        self.defspeed = 3
-        self.createTime = pygame.time.get_ticks()
-
-    def ai_accel(self):
-        """Defines basic movement behaviors for enemies."""
-        # Basic movement behaviors
-        if self.behavior == "stop":
-            self.velx = 0
-            self.vely = 0
-        elif self.behavior == "straightdown":
-            self.velx = 0
-            self.vely = -1 * self.defspeed
-        elif self.behavior == "straightup":
-            self.velx = 0
-            self.vely = self.defspeed
-        elif self.behavior == "straightright":
-            self.velx = self.defspeed
-            self.vely = 0
-        elif self.behavior == "straightleft":
-            self.velx = -1 * self.defspeed
-            self.vely = 0
-        elif self.behavior == "random":
-            margin = 10
-            self.velx = randint(self.defspeed * -1,self.defspeed)
-            self.vely = randint(self.defspeed * -1,self.defspeed)
-        elif self.behavior == "bounce":
-            pass
-        elif self.behavior == "zigzag":
-            pass
+    def __init__(self):
+        pass
 
 class Bullet(Mob):
     """Base class for all bullets."""
-    def __init__(self,posx,posy):
-        Mob.__init__(self,posx,posy)
-        self.width = BULW
-        self.height = BULH
-        self.color = WHITE
-        self.defspeed = 8
-        self.slowfactor += 2
-# Specific class definitions
-
-class SquareEnemy(Enemy):
-    def __init__(self,posx,posy):
-        Enemy.__init__(posx,posy)
-
-class TriEnemy(Enemy):
-    def __init__(self,posx,posy):
-        Enemy.__init__(posx,posy)
-
-class CircEnemy(Enemy):
-    def __init__(self,posx,posy):
-        Enemy.__init__(posx,posy)
+    def __init__(self):
+        pass
