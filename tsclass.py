38d585180374421bf47f1ba786584050d9815fc3
j-crit <joshua.c.guam@gmail.com>
2016-02-10 21:08:30 +1000

    Fleshed out basic functions for animation and set up clear/move/draw loop with dirty rect animation.

--- tsclass.py
+++ tsclass.py
@@ -5,13 +5,13 @@ import pygame
 
 class Mob(pygame.sprite.DirtySprite):
     """Base class for all mobile objects in time-shooter."""
-
-    self.defspeed = 0
-    self.slowfactor = SLOWFACTOR
-    self.height = None
-    self.width = None
-    self.rect = None
-    self.color = None
+    defspeed = 0
+    slowfactor = SLOWFACTOR
+    height = 0
+    width = 0
+    rect = None
+    color = WHITE
+    slow = False
 
     def __init__(self,xpos,ypos,xvel=0,yvel=0):
         super(Mob,self).__init__()
@@ -21,21 +21,39 @@ class Mob(pygame.sprite.DirtySprite):
         self.yvel = yvel
         self.dirty = 2 # All mobs update
 
-    def update(self,slow):
+    def update(self):
+        self.move()
+        self.rect = pygame.Rect((self.xpos,self.ypos,
+                                 self.width,self.height))
+
+    def move(self):
         """Updates xpos and ypos attributes according to xvel and yvel."""
         # Can be called with "slow" to divide velocity by slowfactor
         # to "slow down" time
-        if slow:
-            xpos += (xvel / self.slowfactor)
-            ypos += ((yvel / self.slowfactor) / -1)
+        if self.slow:
+            self.xpos += (self.xvel / self.slowfactor)
+            self.ypos += ((self.yvel / self.slowfactor) / -1)
         else:
-            xpos += xvel
-            ypos += yvel * -1
+            self.xpos += self.xvel
+            self.ypos += self.yvel * -1
+
+    def sketch(self):
+        # Needs to be called during children __init__ to fill
+        # image attribute of sprite.
+        self.image = pygame.Surface((self.width,self.height))
+        self.image.fill(self.color)
+        
             
 class Ship(Mob):
     """Class for the player ship."""
     def __init__(self,xpos,ypos,xvel=0,yvel=0):
         super(Ship,self).__init__(xpos,ypos,xvel,yvel)
+        self.width = 30
+        self.height = 40
+        self.color = WHITE
+        self.defspeed = 8
+        self.sketch()
+        
 
 class Enemy(Mob):
     """Base class for all enemies."""
